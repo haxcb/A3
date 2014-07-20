d14b2ab4973e970eb886560c69883f51a34470c6
Hasiba <hasiba_arshad@hotmail.com>
2014-07-20 15:23:30 -0700

    Uncommented & separated example code

--- js/p2.js
+++ js/p2.js
@@ -1,7 +1,7 @@
-// var svg2 = d3.select(".container").append("svg")
-	// .attr("width", width * 4)
-	// .attr("height", height)
-	// .attr("display", 'none');
+var svg2 = d3.select(".container").append("svg")
+	.attr("width", width * 4)
+	.attr("height", height)
+	.attr("display", 'none');
 
 var showButton = d3.select('button').style('display', 'none');
 
@@ -12,18 +12,20 @@ function buildCallGraph(filteredNodes, hiddenNodes) {
 	visibleNodes = filteredNodes;
 	invisibleNodes = hiddenNodes;
 	showButton.style('display', 'block');
-	showButton.on("click", generateCallGraph);
+	showButton.on("click", exampleGraph);
+	// showButton.on("click", generateCallGraph);
 }
 
 function generateCallGraph() {
-
 	console.log(invisibleNodes);
-	
-       var testData = [
+}
+
+function exampleGraph() {
+    var testData = [
         {label: "person a", times: [{"starting_time": 1355752800000, "ending_time": 1355759900000}, {"starting_time": 1355767900000, "ending_time": 1355774400000}]},
         {label: "person b", times: [{"starting_time": 1355759910000, "ending_time": 1355761900000}, ]},
         {label: "person c", times: [{"starting_time": 1355761910000, "ending_time": 1355763910000}]},
-      ];
+    ];
 	
 	var chart = d3.timeline()
           .stack()
